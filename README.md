874bbe6193583ca108130fed9e94f445d5979dab
gox <gox.roche@gmail.com>
2021-04-04 16:13:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dark-heresy-pdf-dl
---
+++ app.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+
+const serverUrl = "https://www.fillinsheets.com/pdfs/DarkHeresy/";
+const pdfFolderName = "dh-pdf";
+
+let downloadPdfFile = async (serverUrl, pdfUrl) => {
+    const downloadedFileStream = await axios({ url: serverUrl+pdfUrl, method: 'GET', responseType: 'stream' });
+    const filename = pdfUrl.replace("?path=&download=", "").replace(/%20/g, "_");
+    const localFilePath = path.resolve(__dirname, pdfFolderName, filename);
+    await downloadedFileStream.data.pipe(fs.createWriteStream(localFilePath));
+    console.log("Downloaded " + filename);
+}
+
+(async () => {
+    try {
+        if (!fs.existsSync(pdfFolderName)){
+            fs.mkdirSync(pdfFolderName);
+        }
+        const response = await axios.get(serverUrl);
+        const $ = cheerio.load(response.data);
+        let delay = 0;
+
+        $('a').each((i, link) => {
+            const pdfUrl = $(link).attr('href');
+            const paramStr = "?path=&download=";
+            if (pdfUrl.indexOf(paramStr) != -1) {
+                setTimeout(async () => {
+                    await downloadPdfFile(serverUrl, pdfUrl);
+                }, delay);
+                delay += 5000;
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+})();
